97ad4ba4d6b943cee2e5e1ecd2c13fa729a51e87
LionBlue98 <LionBlue98@LAPTOP-1EO3F80H>
2019-10-29 13:29:36 +0000

    Kata2: a medias

---
+++ src/kata2/ConsoleHistogramDisplay.java
@@ -0,0 +1,14 @@
+package kata2;
+
+public class ConsoleHistogramDisplay implements HistogramDisplay {
+
+    
+    public ConsoleHistogramDisplay(){}
+    
+    @Override
+    public void show(Histogram histogram) {
+        for (int value : histogram) {
+            System.out.println(value + " " + histogram.getCount(value));
+        }
+    }
+}
